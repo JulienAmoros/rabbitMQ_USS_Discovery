5874942b1eee6136f22697eb65f2e7f0c5a7abec
Julien AMOROS <julien.amoros@cloud-temple.com>
2018-03-27 10:51:36 +0200

    style change infinity loop to tailf on /dev/null

--- docker/cluster/slave/join_cluster.sh
+++ docker/cluster/slave/join_cluster.sh
@@ -3,8 +3,5 @@ rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$(hostname).pid
 rabbitmqctl stop_app
 rabbitmqctl join_cluster rabbit@master-rabbit
 rabbitmqctl start_app
-#tail -f /var/log/rabbitmq/rabbit*.log
-# Kind of shitty but works, please PR if you have anything better (container shuts down otherwise)
-while true; do
-sleep 10
-done
+#tail -f /some/log/file.log
+tail -f /dev/null
